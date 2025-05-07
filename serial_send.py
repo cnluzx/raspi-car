bc8e04dcba9a0a76e522be46898cbb627e9594e5
cnluzx <cnluzx@outlook.com>
2025-05-07 12:26:38 +0800

    666

--- serial_send.py
+++ serial_send.py
@@ -16,12 +16,16 @@ def read_data():
 def move_ahead():
     send_data("1|60|0")
 
+###根据下位机的指示
+##param1:指令类型 1:前进 2:转向
+##param2:角度值  负数表示左转，正数表示右转度数
+##param3:速度值  范围0-100
 
 def turn_left():
-    send_data("2|25|90")
+    send_data("2|90|25")
 
 def turn_right():
-    send_data("2|25|180")
+    send_data("2|-90|25")
 
 def stop():
     send_data("1|0|0")
